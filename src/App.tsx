8609555f87898d15dac5878bfa778a4f217a25bc
Арсений Стефанов <sirsenya@Arsenijs-MacBook-Pro.local>
2026-09-05 09:54:39 +0300

    add mobka v 0.1

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import { ReligionGame } from "./religion/ReligionGame";
-
-function App() {
-  return <ReligionGame />;
-}
-
-export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: './',
+  server: {
+    port: 5173,
+    host: true,
+  },
+  build: {
+    target: 'es2020',
+    chunkSizeWarningLimit: 2000,
+  },
+});
